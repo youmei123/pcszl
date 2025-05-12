0e9598f9a5433fa17e2a590ff35b2ef20c8cb699
Lzx <924807479@qq.com>
2025-05-12 17:47:36 +0800

    提交修改

--- pcszl/src/views/usercenter/menuitem/mycourse/index.vue
+++ pcszl/src/views/usercenter/menuitem/mycourse/index.vue
@@ -2,7 +2,7 @@
  * @Author: Lzx 924807479@qq.com
  * @Date: 2025-04-14 16:28:42
  * @LastEditors: Lzx 924807479@qq.com
- * @LastEditTime: 2025-05-12 17:41:41
+ * @LastEditTime: 2025-05-12 17:47:05
  * @FilePath: \pcszl\src\views\usercenter\menuitem\mycourse\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
