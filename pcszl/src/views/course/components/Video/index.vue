8202954af50b64d9a43ef0920728034a8ac3f2c0
Lzx <924807479@qq.com>
2025-05-20 15:54:09 +0800

    1

--- pcszl/src/views/course/components/Video/index.vue
+++ pcszl/src/views/course/components/Video/index.vue
@@ -2,7 +2,7 @@
  * @Author: Lzx 924807479@qq.com
  * @Date: 2025-04-11 16:03:51
  * @LastEditors: Lzx 924807479@qq.com
- * @LastEditTime: 2025-05-20 15:40:06
+ * @LastEditTime: 2025-05-20 15:52:45
  * @FilePath: \pcszl\src\views\course\components\Video\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -46,10 +46,6 @@ const props = defineProps({
   poster: {
     type: String,
     default: "",
-  },
-  ispay: {
-    type: Boolean,
-    default: false,
   },
 });
 
